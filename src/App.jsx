fedf16293ef869ace5912d7e1d202ef11ce562b2
𝘼𝙫𝙞𝙣𝙖𝙨𝙝 <avinashk08408@gmail.com>
2026-08-28 21:30:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 /** Quiet Scholar component composition entry. */
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -11,7 +14,9 @@ import Activity from "./components/Activity/Activity";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
-export default function App() {
+import ProjectsPage from "./pages/ProjectsPage";
+
+function HomePage() {
   return (
     <div className="quiet-scholar content-edition">
       <Navbar />
@@ -32,3 +37,22 @@ export default function App() {
     </div>
   );
 }
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Main Portfolio */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* All Projects Page */}
+        <Route path="/projects" element={<ProjectsPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
